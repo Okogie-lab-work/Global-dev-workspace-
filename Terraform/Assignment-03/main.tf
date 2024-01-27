1aef86b0e8bfc288ff96a4dfce825823f0943a74
wilfred okogie <globalokogiestaff@gmail.com>
2024-01-27 09:28:37 +0000

    update#

--- Terraform/Assignment-03/main.tf
+++ Terraform/Assignment-03/main.tf
@@ -22,19 +22,21 @@ resource "aws_instance" "nginx_server" {
 
 resource "aws_security_group" "nginx_security_group" {
   name        = "nginx_security_group"
-  description = "Allow HTTP"
+  description = "Allow ALL TCP"
+
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
