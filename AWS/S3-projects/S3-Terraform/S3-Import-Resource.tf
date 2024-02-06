350a2364528260766ff0f9edcffbabca553aedb5
wilfred okogie <globalokogiestaff@gmail.com>
2024-02-06 15:38:15 +0000

    update terraform file

---
+++ AWS/S3-projects/S3-Terraform/S3-Import-Resource.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "global-s3-project-okogie"
+ public_access_block_configuration {
+    block_public_acls   = false
+    block_public_policy = false
+  }
+}
+
+
+resource "aws_s3_bucket_object" "object1" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "Cloud-Engineer-Jobs_Blog-2048x1158.jpeg"
+}
+
+resource "aws_s3_bucket_object" "object2" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "OIP (1).jpeg"
+}
+
+resource "aws_s3_bucket_object" "object3" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "Staticweb.html"
+}
